9271aa0078b3c734255ea7fd36db7e5fd6034087
leftthomas <leftthomas@qq.com>
2018-05-13 21:25:01 +0800

    simplified code

--- model.py
+++ model.py
@@ -4,55 +4,173 @@ import torch.nn as nn
 from utils import VirtualBatchNorm1d
 
 
+class Generator(nn.Module):
+    """G"""
+
+    def __init__(self):
+        super().__init__()
+        # encoder gets a noisy signal as input [B x 1 x 16384]
+        self.enc1 = nn.Conv1d(in_channels=1, out_channels=16, kernel_size=32, stride=2, padding=15)  # [B x 16 x 8192]
+        self.enc1_nl = nn.PReLU()
+        self.enc2 = nn.Conv1d(16, 32, 32, 2, 15)  # [B x 32 x 4096]
+        self.enc2_nl = nn.PReLU()
+        self.enc3 = nn.Conv1d(32, 32, 32, 2, 15)  # [B x 32 x 2048]
+        self.enc3_nl = nn.PReLU()
+        self.enc4 = nn.Conv1d(32, 64, 32, 2, 15)  # [B x 64 x 1024]
+        self.enc4_nl = nn.PReLU()
+        self.enc5 = nn.Conv1d(64, 64, 32, 2, 15)  # [B x 64 x 512]
+        self.enc5_nl = nn.PReLU()
+        self.enc6 = nn.Conv1d(64, 128, 32, 2, 15)  # [B x 128 x 256]
+        self.enc6_nl = nn.PReLU()
+        self.enc7 = nn.Conv1d(128, 128, 32, 2, 15)  # [B x 128 x 128]
+        self.enc7_nl = nn.PReLU()
+        self.enc8 = nn.Conv1d(128, 256, 32, 2, 15)  # [B x 256 x 64]
+        self.enc8_nl = nn.PReLU()
+        self.enc9 = nn.Conv1d(256, 256, 32, 2, 15)  # [B x 256 x 32]
+        self.enc9_nl = nn.PReLU()
+        self.enc10 = nn.Conv1d(256, 512, 32, 2, 15)  # [B x 512 x 16]
+        self.enc10_nl = nn.PReLU()
+        self.enc11 = nn.Conv1d(512, 1024, 32, 2, 15)  # [B x 1024 x 8]
+        self.enc11_nl = nn.PReLU()
+
+        # decoder generates an enhanced signal
+        # each decoder output are concatenated with homologous encoder output,
+        # so the feature map sizes are doubled
+        self.dec10 = nn.ConvTranspose1d(in_channels=2048, out_channels=512, kernel_size=32, stride=2, padding=15)
+        self.dec10_nl = nn.PReLU()  # out : [B x 512 x 16] -> (concat) [B x 1024 x 16]
+        self.dec9 = nn.ConvTranspose1d(1024, 256, 32, 2, 15)  # [B x 256 x 32]
+        self.dec9_nl = nn.PReLU()
+        self.dec8 = nn.ConvTranspose1d(512, 256, 32, 2, 15)  # [B x 256 x 64]
+        self.dec8_nl = nn.PReLU()
+        self.dec7 = nn.ConvTranspose1d(512, 128, 32, 2, 15)  # [B x 128 x 128]
+        self.dec7_nl = nn.PReLU()
+        self.dec6 = nn.ConvTranspose1d(256, 128, 32, 2, 15)  # [B x 128 x 256]
+        self.dec6_nl = nn.PReLU()
+        self.dec5 = nn.ConvTranspose1d(256, 64, 32, 2, 15)  # [B x 64 x 512]
+        self.dec5_nl = nn.PReLU()
+        self.dec4 = nn.ConvTranspose1d(128, 64, 32, 2, 15)  # [B x 64 x 1024]
+        self.dec4_nl = nn.PReLU()
+        self.dec3 = nn.ConvTranspose1d(128, 32, 32, 2, 15)  # [B x 32 x 2048]
+        self.dec3_nl = nn.PReLU()
+        self.dec2 = nn.ConvTranspose1d(64, 32, 32, 2, 15)  # [B x 32 x 4096]
+        self.dec2_nl = nn.PReLU()
+        self.dec1 = nn.ConvTranspose1d(64, 16, 32, 2, 15)  # [B x 16 x 8192]
+        self.dec1_nl = nn.PReLU()
+        self.dec_final = nn.ConvTranspose1d(32, 1, 32, 2, 15)  # [B x 1 x 16384]
+        self.dec_tanh = nn.Tanh()
+
+        # initialize weights
+        self.init_weights()
+
+    def init_weights(self):
+        """
+        Initialize weights for convolution layers using Xavier initialization.
+        """
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d) or isinstance(m, nn.ConvTranspose1d):
+                nn.init.xavier_normal(m.weight.data)
+
+    def forward(self, x, z):
+        """
+        Forward pass of generator.
+
+        Args:
+            x: input batch (signal)
+            z: latent vector
+        """
+        # encoding step
+        e1 = self.enc1(x)
+        e2 = self.enc2(self.enc1_nl(e1))
+        e3 = self.enc3(self.enc2_nl(e2))
+        e4 = self.enc4(self.enc3_nl(e3))
+        e5 = self.enc5(self.enc4_nl(e4))
+        e6 = self.enc6(self.enc5_nl(e5))
+        e7 = self.enc7(self.enc6_nl(e6))
+        e8 = self.enc8(self.enc7_nl(e7))
+        e9 = self.enc9(self.enc8_nl(e8))
+        e10 = self.enc10(self.enc9_nl(e9))
+        e11 = self.enc11(self.enc10_nl(e10))
+        # c = compressed feature, the 'thought vector'
+        c = self.enc11_nl(e11)
+
+        # concatenate the thought vector with latent variable
+        encoded = torch.cat((c, z), dim=1)
+
+        # decoding step
+        d10 = self.dec10(encoded)
+        # dx_c : concatenated with skip-connected layer's output & passed nonlinear layer
+        d10_c = self.dec10_nl(torch.cat((d10, e10), dim=1))
+        d9 = self.dec9(d10_c)
+        d9_c = self.dec9_nl(torch.cat((d9, e9), dim=1))
+        d8 = self.dec8(d9_c)
+        d8_c = self.dec8_nl(torch.cat((d8, e8), dim=1))
+        d7 = self.dec7(d8_c)
+        d7_c = self.dec7_nl(torch.cat((d7, e7), dim=1))
+        d6 = self.dec6(d7_c)
+        d6_c = self.dec6_nl(torch.cat((d6, e6), dim=1))
+        d5 = self.dec5(d6_c)
+        d5_c = self.dec5_nl(torch.cat((d5, e5), dim=1))
+        d4 = self.dec4(d5_c)
+        d4_c = self.dec4_nl(torch.cat((d4, e4), dim=1))
+        d3 = self.dec3(d4_c)
+        d3_c = self.dec3_nl(torch.cat((d3, e3), dim=1))
+        d2 = self.dec2(d3_c)
+        d2_c = self.dec2_nl(torch.cat((d2, e2), dim=1))
+        d1 = self.dec1(d2_c)
+        d1_c = self.dec1_nl(torch.cat((d1, e1), dim=1))
+        out = self.dec_tanh(self.dec_final(d1_c))
+        return out
+
+
 class Discriminator(nn.Module):
     """D"""
-    def __init__(self, dropout_drop=0.5):
+
+    def __init__(self, dropout_rate=0.5):
         super().__init__()
-        # Define convolution operations.
-        # (#input channel, #output channel, kernel_size, stride, padding)
-        # in : 16384 x 2
+        # D gets a noisy signal and clear signal as input [B x 2 x 16384]
         negative_slope = 0.03
-        self.conv1 = nn.Conv1d(in_channels=2, out_channels=32, kernel_size=31, stride=2, padding=15)   # out : 8192 x 32
+        self.conv1 = nn.Conv1d(in_channels=2, out_channels=32, kernel_size=31, stride=2, padding=15)  # [B x 32 x 8192]
         self.vbn1 = VirtualBatchNorm1d(32)
         self.lrelu1 = nn.LeakyReLU(negative_slope)
-        self.conv2 = nn.Conv1d(32, 64, 31, 2, 15)  # 4096 x 64
+        self.conv2 = nn.Conv1d(32, 64, 31, 2, 15)  # [B x 64 x 4096]
         self.vbn2 = VirtualBatchNorm1d(64)
         self.lrelu2 = nn.LeakyReLU(negative_slope)
-        self.conv3 = nn.Conv1d(64, 64, 31, 2, 15)  # 2048 x 64
-        self.dropout1 = nn.Dropout(dropout_drop)
+        self.conv3 = nn.Conv1d(64, 64, 31, 2, 15)  # [B x 64 x 2048]
+        self.dropout1 = nn.Dropout(dropout_rate)
         self.vbn3 = VirtualBatchNorm1d(64)
         self.lrelu3 = nn.LeakyReLU(negative_slope)
-        self.conv4 = nn.Conv1d(64, 128, 31, 2, 15) # 1024 x 128
+        self.conv4 = nn.Conv1d(64, 128, 31, 2, 15)  # [B x 128 x 1024]
         self.vbn4 = VirtualBatchNorm1d(128)
         self.lrelu4 = nn.LeakyReLU(negative_slope)
-        self.conv5 = nn.Conv1d(128, 128, 31, 2, 15)  # 512 x 128
+        self.conv5 = nn.Conv1d(128, 128, 31, 2, 15)  # [B x 128 x 512]
         self.vbn5 = VirtualBatchNorm1d(128)
         self.lrelu5 = nn.LeakyReLU(negative_slope)
-        self.conv6 = nn.Conv1d(128, 256, 31, 2, 15)  # 256 x 256
-        self.dropout2 = nn.Dropout(dropout_drop)
+        self.conv6 = nn.Conv1d(128, 256, 31, 2, 15)  # [B x 256 x 256]
+        self.dropout2 = nn.Dropout(dropout_rate)
         self.vbn6 = VirtualBatchNorm1d(256)
         self.lrelu6 = nn.LeakyReLU(negative_slope)
-        self.conv7 = nn.Conv1d(256, 256, 31, 2, 15)  # 128 x 256
+        self.conv7 = nn.Conv1d(256, 256, 31, 2, 15)  # [B x 256 x 128]
         self.vbn7 = VirtualBatchNorm1d(256)
         self.lrelu7 = nn.LeakyReLU(negative_slope)
-        self.conv8 = nn.Conv1d(256, 512, 31, 2, 15)  # 64 x 512
+        self.conv8 = nn.Conv1d(256, 512, 31, 2, 15)  # [B x 512 x 64]
         self.vbn8 = VirtualBatchNorm1d(512)
         self.lrelu8 = nn.LeakyReLU(negative_slope)
-        self.conv9 = nn.Conv1d(512, 512, 31, 2, 15)  # 32 x 512
-        self.dropout3 = nn.Dropout(dropout_drop)
+        self.conv9 = nn.Conv1d(512, 512, 31, 2, 15)  # [B x 512 x 32]
+        self.dropout3 = nn.Dropout(dropout_rate)
         self.vbn9 = VirtualBatchNorm1d(512)
         self.lrelu9 = nn.LeakyReLU(negative_slope)
-        self.conv10 = nn.Conv1d(512, 1024, 31, 2, 15)  # 16 x 1024
+        self.conv10 = nn.Conv1d(512, 1024, 31, 2, 15)  # [B x 1024 x 16]
         self.vbn10 = VirtualBatchNorm1d(1024)
         self.lrelu10 = nn.LeakyReLU(negative_slope)
-        self.conv11 = nn.Conv1d(1024, 2048, 31, 2, 15)  # 8 x 1024
+        self.conv11 = nn.Conv1d(1024, 2048, 31, 2, 15)  # [B x 2048 x 8]
         self.vbn11 = VirtualBatchNorm1d(2048)
         self.lrelu11 = nn.LeakyReLU(negative_slope)
         # 1x1 size kernel for dimension and parameter reduction
-        self.conv_final = nn.Conv1d(2048, 1, kernel_size=1, stride=1)  # 8 x 1
+        self.conv_final = nn.Conv1d(2048, 1, kernel_size=1, stride=1)  # [B x 1 x 8]
         self.lrelu_final = nn.LeakyReLU(negative_slope)
-        self.fully_connected = nn.Linear(in_features=8, out_features=1)  # 1
+        self.fully_connected = nn.Linear(in_features=8, out_features=1)  # [B x 1]
         self.sigmoid = nn.Sigmoid()
+
         # initialize weights
         self.init_weights()
 
@@ -69,8 +187,8 @@ class Discriminator(nn.Module):
         Forward pass of discriminator.
 
         Args:
-            x: batch
-            ref_x: reference batch for virtual batch norm
+            x: input batch (signal)
+            ref_x: reference input batch for virtual batch norm
         """
         # reference pass
         ref_x = self.conv1(ref_x)
@@ -153,121 +271,3 @@ class Discriminator(nn.Module):
         x = torch.squeeze(x)
         x = self.fully_connected(x)
         return self.sigmoid(x)
-
-
-class Generator(nn.Module):
-    """G"""
-    def __init__(self, batch_size):
-        super().__init__()
-        # size notations = [batch_size x feature_maps x width] (height omitted - 1D convolutions)
-        # encoder gets a noisy signal as input
-        self.enc1 = nn.Conv1d(in_channels=1, out_channels=16, kernel_size=32, stride=2, padding=15)   # out : [B x 16 x 8192]
-        self.enc1_nl = nn.PReLU()  # non-linear transformation after encoder layer 1
-        self.enc2 = nn.Conv1d(16, 32, 32, 2, 15)  # [B x 32 x 4096]
-        self.enc2_nl = nn.PReLU()
-        self.enc3 = nn.Conv1d(32, 32, 32, 2, 15)  # [B x 32 x 2048]
-        self.enc3_nl = nn.PReLU()
-        self.enc4 = nn.Conv1d(32, 64, 32, 2, 15)  # [B x 64 x 1024]
-        self.enc4_nl = nn.PReLU()
-        self.enc5 = nn.Conv1d(64, 64, 32, 2, 15)  # [B x 64 x 512]
-        self.enc5_nl = nn.PReLU()
-        self.enc6 = nn.Conv1d(64, 128, 32, 2, 15)  # [B x 128 x 256]
-        self.enc6_nl = nn.PReLU()
-        self.enc7 = nn.Conv1d(128, 128, 32, 2, 15)  # [B x 128 x 128]
-        self.enc7_nl = nn.PReLU()
-        self.enc8 = nn.Conv1d(128, 256, 32, 2, 15)  # [B x 256 x 64]
-        self.enc8_nl = nn.PReLU()
-        self.enc9 = nn.Conv1d(256, 256, 32, 2, 15)  # [B x 256 x 32]
-        self.enc9_nl = nn.PReLU()
-        self.enc10 = nn.Conv1d(256, 512, 32, 2, 15)  # [B x 512 x 16]
-        self.enc10_nl = nn.PReLU()
-        self.enc11 = nn.Conv1d(512, 1024, 32, 2, 15)  # output : [B x 1024 x 8]
-        self.enc11_nl = nn.PReLU()
-
-        # decoder generates an enhanced signal
-        # each decoder output are concatenated with homolgous encoder output,
-        # so the feature map sizes are doubled
-        self.dec10 = nn.ConvTranspose1d(in_channels=2048, out_channels=512, kernel_size=32, stride=2, padding=15)
-        self.dec10_nl = nn.PReLU()  # out : [B x 512 x 16] -> (concat) [B x 1024 x 16]
-        self.dec9 = nn.ConvTranspose1d(1024, 256, 32, 2, 15)  # [B x 256 x 32]
-        self.dec9_nl = nn.PReLU()
-        self.dec8 = nn.ConvTranspose1d(512, 256, 32, 2, 15)  # [B x 256 x 64]
-        self.dec8_nl = nn.PReLU()
-        self.dec7 = nn.ConvTranspose1d(512, 128, 32, 2, 15)  # [B x 128 x 128]
-        self.dec7_nl = nn.PReLU()
-        self.dec6 = nn.ConvTranspose1d(256, 128, 32, 2, 15)  # [B x 128 x 256]
-        self.dec6_nl = nn.PReLU()
-        self.dec5 = nn.ConvTranspose1d(256, 64, 32, 2, 15)  # [B x 64 x 512]
-        self.dec5_nl = nn.PReLU()
-        self.dec4 = nn.ConvTranspose1d(128, 64, 32, 2, 15)  # [B x 64 x 1024]
-        self.dec4_nl = nn.PReLU()
-        self.dec3 = nn.ConvTranspose1d(128, 32, 32, 2, 15)  # [B x 32 x 2048]
-        self.dec3_nl = nn.PReLU()
-        self.dec2 = nn.ConvTranspose1d(64, 32, 32, 2, 15)  # [B x 32 x 4096]
-        self.dec2_nl = nn.PReLU()
-        self.dec1 = nn.ConvTranspose1d(64, 16, 32, 2, 15)  # [B x 16 x 8192]
-        self.dec1_nl = nn.PReLU()
-        self.dec_final = nn.ConvTranspose1d(32, 1, 32, 2, 15)  # [B x 1 x 16384]
-        self.dec_tanh = nn.Tanh()
-
-        # initialize weights
-        self.init_weights()
-
-    def init_weights(self):
-        """
-        Initialize weights for convolution layers using Xavier initialization.
-        """
-        for m in self.modules():
-            if isinstance(m, nn.Conv1d) or isinstance(m, nn.ConvTranspose1d):
-                nn.init.xavier_normal(m.weight.data)
-
-    def forward(self, x, z):
-        """
-        Forward pass of generator.
-
-        Args:
-            x: input batch (signal)
-            z: latent vector
-        """
-        ### encoding step
-        e1 = self.enc1(x)
-        e2 = self.enc2(self.enc1_nl(e1))
-        e3 = self.enc3(self.enc2_nl(e2))
-        e4 = self.enc4(self.enc3_nl(e3))
-        e5 = self.enc5(self.enc4_nl(e4))
-        e6 = self.enc6(self.enc5_nl(e5))
-        e7 = self.enc7(self.enc6_nl(e6))
-        e8 = self.enc8(self.enc7_nl(e7))
-        e9 = self.enc9(self.enc8_nl(e8))
-        e10 = self.enc10(self.enc9_nl(e9))
-        e11 = self.enc11(self.enc10_nl(e10))
-        # c = compressed feature, the 'thought vector'
-        c = self.enc11_nl(e11)
-
-        # concatenate the thought vector with latent variable
-        encoded = torch.cat((c, z), dim=1)
-
-        ### decoding step
-        d10 = self.dec10(encoded)
-        # dx_c : concatenated with skip-connected layer's output & passed nonlinear layer
-        d10_c = self.dec10_nl(torch.cat((d10, e10), dim=1))
-        d9 = self.dec9(d10_c)
-        d9_c = self.dec9_nl(torch.cat((d9, e9), dim=1))
-        d8 = self.dec8(d9_c)
-        d8_c = self.dec8_nl(torch.cat((d8, e8), dim=1))
-        d7 = self.dec7(d8_c)
-        d7_c = self.dec7_nl(torch.cat((d7, e7), dim=1))
-        d6 = self.dec6(d7_c)
-        d6_c = self.dec6_nl(torch.cat((d6, e6), dim=1))
-        d5 = self.dec5(d6_c)
-        d5_c = self.dec5_nl(torch.cat((d5, e5), dim=1))
-        d4 = self.dec4(d5_c)
-        d4_c = self.dec4_nl(torch.cat((d4, e4), dim=1))
-        d3 = self.dec3(d4_c)
-        d3_c = self.dec3_nl(torch.cat((d3, e3), dim=1))
-        d2 = self.dec2(d3_c)
-        d2_c = self.dec2_nl(torch.cat((d2, e2), dim=1))
-        d1 = self.dec1(d2_c)
-        d1_c = self.dec1_nl(torch.cat((d1, e1), dim=1))
-        out = self.dec_tanh(self.dec_final(d1_c))
-        return out
